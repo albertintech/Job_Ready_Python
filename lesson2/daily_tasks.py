d819d86c71c3289eb95c5801df5088e854bc925b
Albert Ramos <albertramos.dev@gmail.com>
2021-12-15 16:05:25 -0700

    Add exercise files

---
+++ lesson2/daily_tasks.py
@@ -0,0 +1,8 @@
+# As you go through routine tasks over the next few days, think through and define algorithms for those tasks. Consider writing the steps out for the following tasks:
+# Tie shoes
+# Load a dishwasher
+# Make a bed
+# Create a peanut butter and jelly sandwich
+# How would you write the steps for these tasks for a computer? Write the steps and see if another person can follow them exactly as they are written.
+
+# Balti, Haythem; Weiss, Kimberly A.. Job Ready Python (p. 47). Wiley. Kindle Edition. 
