ec20fb33bedd9240c6769d3202dc219d77cb55b3
Albert Ramos <albertramos.dev@gmail.com>
2021-12-21 17:35:25 -0700

    Add solution

---
+++ lesson4/manipulated_math.py
@@ -0,0 +1,10 @@
+# Update the following code so that the result is equal to 576. Do not change any of the existing values or operators or the order in which they appear. 
+
+# do not change the order in which the numbers and 
+# operators appear in the next line
+ 
+result = ((5 + 3) ** 2) * 9
+ 
+print(result) # the output should be 576
+
+# Balti, Haythem; Weiss, Kimberly A.. Job Ready Python (p. 97). Wiley. Kindle Edition. 
